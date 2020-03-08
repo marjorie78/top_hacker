5ab873cc362ba84f3af69d44a8ab6e3edae306a6
itb-ie <42895988+itb-ie@users.noreply.github.com>
2020-03-08 23:19:10 +0100

    initial commit

---
+++ decode.py
@@ -0,0 +1,16 @@
+alpha = " abcdefghijklmnopqrstuvwxyz"
+vowel = list("aeiou")
+
+fp = open("secret.txt")
+decoded = ""  # this is your decoded message. It gets one letter per line of file decoded
+
+#start adding code here
+
+
+
+
+
+
+#end the code here
+print(decoded)
+fp.close()
